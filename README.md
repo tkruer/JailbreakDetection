31f2a8b2bfc2a482a595188fbdd2849f43ed3042
Tyler K <tylerkruer1@gmail.com>
2022-10-17 20:01:44 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JailbreakDetection
+
+A description of this package.
---
+++ Sources/JailbreakDetection/JailbreakDetection.swift
@@ -0,0 +1,6 @@
+public struct JailbreakDetection {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
