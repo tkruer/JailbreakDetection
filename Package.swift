2500b417aa761cbeec5f65db96344de86c5c9ffb
Tyler K <tylerkruer1@gmail.com>
2022-10-18 08:40:42 -0700

    fixed target

--- Package.swift
+++ Package.swift
@@ -26,7 +26,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JailbreakDetection",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "JailbreakDetectionTests",
             dependencies: ["JailbreakDetection"]),
